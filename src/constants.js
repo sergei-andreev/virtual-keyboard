2281c5edb081c95f1cb2828e05812e72a0271e13
Sergei Andreev <sergey.andreev@dualbootpartners.com>
2023-04-29 12:07:52 +0200

    feat: add shift and capslock functional

--- src/constants.js
+++ src/constants.js
@@ -1,212 +1,232 @@
-export default {
+export const KEYBOARD_KEYS = {
   firstRow: [
     {
-      en: '`', ru: 'ё', code: 'Backquote', shiftEn: '~', shiftRu: 'Ё',
+      en: '`', ru: 'ё', code: 'Backquote', enShift: '~', ruShift: 'Ё',
     },
     {
-      en: '1', ru: '1', code: 'Digit1', shiftEn: '!', shiftRu: '!',
+      en: '1', ru: '1', code: 'Digit1', enShift: '!', ruShift: '!',
     },
     {
-      en: '2', ru: '2', code: 'Digit2', shiftEn: '@', shiftRu: '"',
+      en: '2', ru: '2', code: 'Digit2', enShift: '@', ruShift: '"',
     },
     {
-      en: '3', ru: '3', code: 'Digit3', shiftEn: '#', shiftRu: '№',
+      en: '3', ru: '3', code: 'Digit3', enShift: '#', ruShift: '№',
     },
     {
-      en: '4', ru: '4', code: 'Digit4', shiftEn: '$', shiftRu: ';',
+      en: '4', ru: '4', code: 'Digit4', enShift: '$', ruShift: ';',
     },
     {
-      en: '5', ru: '5', code: 'Digit5', shiftEn: '%', shiftRu: '%',
+      en: '5', ru: '5', code: 'Digit5', enShift: '%', ruShift: '%',
     },
     {
-      en: '6', ru: '6', code: 'Digit6', shiftEn: '^', shiftRu: ':',
+      en: '6', ru: '6', code: 'Digit6', enShift: '^', ruShift: ':',
     },
     {
-      en: '7', ru: '7', code: 'Digit7', shiftEn: '&', shiftRu: '?',
+      en: '7', ru: '7', code: 'Digit7', enShift: '&', ruShift: '?',
     },
     {
-      en: '8', ru: '8', code: 'Digit8', shiftEn: '*', shiftRu: '*',
+      en: '8', ru: '8', code: 'Digit8', enShift: '*', ruShift: '*',
     },
     {
-      en: '9', ru: '9', code: 'Digit9', shiftEn: '(', shiftRu: '(',
+      en: '9', ru: '9', code: 'Digit9', enShift: '(', ruShift: '(',
     },
     {
-      en: '0', ru: '0', code: 'Digit0', shiftEn: ')', shiftRu: ')',
+      en: '0', ru: '0', code: 'Digit0', enShift: ')', ruShift: ')',
     },
     {
-      en: '-', ru: '-', code: 'Minus', shiftEn: '_', shiftRu: '_',
+      en: '-', ru: '-', code: 'Minus', enShift: '_', ruShift: '_',
     },
     {
-      en: '=', ru: '=', code: 'Equal', shiftEn: '+', shiftRu: '+',
+      en: '=', ru: '=', code: 'Equal', enShift: '+', ruShift: '+',
     },
     {
-      en: 'Backspace', ru: 'Backspace', code: 'Backspace', shiftEn: 'Backspace', shiftRu: 'Backspace',
+      en: 'Backspace', ru: 'Backspace', code: 'Backspace', enShift: 'Backspace', ruShift: 'Backspace',
     },
   ],
 
   secondRow: [
     {
-      en: 'Tab', ru: 'Tab', code: 'Tab', shiftEn: 'Tab', shiftRu: 'Tab',
+      en: 'Tab', ru: 'Tab', code: 'Tab', enShift: 'Tab', ruShift: 'Tab',
     },
     {
-      en: 'q', ru: 'й', code: 'KeyQ', shiftEn: 'Q', shiftRu: 'Й',
+      en: 'q', ru: 'й', code: 'KeyQ', enShift: 'Q', ruShift: 'Й',
     },
     {
-      en: 'w', ru: 'ц', code: 'KeyW', shiftEn: 'W', shiftRu: 'Ц',
+      en: 'w', ru: 'ц', code: 'KeyW', enShift: 'W', ruShift: 'Ц',
     },
     {
-      en: 'e', ru: 'у', code: 'KeyE', shiftEn: 'E', shiftRu: 'У',
+      en: 'e', ru: 'у', code: 'KeyE', enShift: 'E', ruShift: 'У',
     },
     {
-      en: 'r', ru: 'к', code: 'KeyR', shiftEn: 'R', shiftRu: 'К',
+      en: 'r', ru: 'к', code: 'KeyR', enShift: 'R', ruShift: 'К',
     },
     {
-      en: 't', ru: 'е', code: 'KeyT', shiftEn: 'T', shiftRu: 'Е',
+      en: 't', ru: 'е', code: 'KeyT', enShift: 'T', ruShift: 'Е',
     },
     {
-      en: 'y', ru: 'н', code: 'KeyY', shiftEn: 'Y', shiftRu: 'Н',
+      en: 'y', ru: 'н', code: 'KeyY', enShift: 'Y', ruShift: 'Н',
     },
     {
-      en: 'u', ru: 'г', code: 'KeyU', shiftEn: 'U', shiftRu: 'Г',
+      en: 'u', ru: 'г', code: 'KeyU', enShift: 'U', ruShift: 'Г',
     },
     {
-      en: 'i', ru: 'ш', code: 'KeyI', shiftEn: 'I', shiftRu: 'Ш',
+      en: 'i', ru: 'ш', code: 'KeyI', enShift: 'I', ruShift: 'Ш',
     },
     {
-      en: 'o', ru: 'щ', code: 'KeyO', shiftEn: 'O', shiftRu: 'Щ',
+      en: 'o', ru: 'щ', code: 'KeyO', enShift: 'O', ruShift: 'Щ',
     },
     {
-      en: 'p', ru: 'з', code: 'KeyP', shiftEn: 'P', shiftRu: 'З',
+      en: 'p', ru: 'з', code: 'KeyP', enShift: 'P', ruShift: 'З',
     },
     {
-      en: '[', ru: 'х', code: 'BracketLeft', shiftEn: '{', shiftRu: 'Х',
+      en: '[', ru: 'х', code: 'BracketLeft', enShift: '{', ruShift: 'Х',
     },
     {
-      en: ']', ru: 'ъ', code: 'BracketRight', shiftEn: '}', shiftRu: 'Ъ',
+      en: ']', ru: 'ъ', code: 'BracketRight', enShift: '}', ruShift: 'Ъ',
     },
     {
-      en: '\\', ru: '\\', code: 'Backslash', shiftEn: '|', shiftRu: '/',
+      en: '\\', ru: '\\', code: 'Backslash', enShift: '|', ruShift: '/',
     },
   ],
 
   thirdRow: [
     {
-      en: 'CapsLock', ru: 'CapsLock', code: 'CapsLock', shiftEn: 'CapsLock', shiftRu: 'CapsLock',
+      en: 'CapsLock', ru: 'CapsLock', code: 'CapsLock', enShift: 'CapsLock', ruShift: 'CapsLock',
     },
     {
-      en: 'a', ru: 'ф', code: 'KeyA', shiftEn: 'A', shiftRu: 'Ф',
+      en: 'a', ru: 'ф', code: 'KeyA', enShift: 'A', ruShift: 'Ф',
     },
     {
-      en: 's', ru: 'ы', code: 'KeyS', shiftEn: 'S', shiftRu: 'Ы',
+      en: 's', ru: 'ы', code: 'KeyS', enShift: 'S', ruShift: 'Ы',
     },
     {
-      en: 'd', ru: 'в', code: 'KeyD', shiftEn: 'D', shiftRu: 'В',
+      en: 'd', ru: 'в', code: 'KeyD', enShift: 'D', ruShift: 'В',
     },
     {
-      en: 'f', ru: 'а', code: 'KeyF', shiftEn: 'F', shiftRu: 'А',
+      en: 'f', ru: 'а', code: 'KeyF', enShift: 'F', ruShift: 'А',
     },
     {
-      en: 'g', ru: 'п', code: 'KeyG', shiftEn: 'G', shiftRu: 'П',
+      en: 'g', ru: 'п', code: 'KeyG', enShift: 'G', ruShift: 'П',
     },
     {
-      en: 'h', ru: 'р', code: 'KeyH', shiftEn: 'H', shiftRu: 'Р',
+      en: 'h', ru: 'р', code: 'KeyH', enShift: 'H', ruShift: 'Р',
     },
     {
-      en: 'j', ru: 'о', code: 'KeyJ', shiftEn: 'J', shiftRu: 'О',
+      en: 'j', ru: 'о', code: 'KeyJ', enShift: 'J', ruShift: 'О',
     },
     {
-      en: 'k', ru: 'л', code: 'KeyK', shiftEn: 'K', shiftRu: 'Л',
+      en: 'k', ru: 'л', code: 'KeyK', enShift: 'K', ruShift: 'Л',
     },
     {
-      en: 'l', ru: 'д', code: 'KeyL', shiftEn: 'L', shiftRu: 'Д',
+      en: 'l', ru: 'д', code: 'KeyL', enShift: 'L', ruShift: 'Д',
     },
     {
-      en: ';', ru: 'ж', code: 'Semicolon', shiftEn: ':', shiftRu: 'Ж',
+      en: ';', ru: 'ж', code: 'Semicolon', enShift: ':', ruShift: 'Ж',
     },
     {
-      en: '\'', ru: 'э', code: 'Quote', shiftEn: '"', shiftRu: 'Э',
+      en: '\'', ru: 'э', code: 'Quote', enShift: '"', ruShift: 'Э',
     },
     {
-      en: 'Enter', ru: 'Enter', code: 'Enter', shiftEn: 'Enter', shiftRu: 'Enter',
+      en: 'Enter', ru: 'Enter', code: 'Enter', enShift: 'Enter', ruShift: 'Enter',
     },
   ],
 
   fourthRow: [
     {
-      en: 'Shift', ru: 'Shift', code: 'ShiftLeft', shiftEn: 'Shift', shiftRu: 'Shift',
+      en: 'Shift', ru: 'Shift', code: 'ShiftLeft', enShift: 'Shift', ruShift: 'Shift',
     },
     {
-      en: 'z', ru: 'я', code: 'KeyZ', shiftEn: 'Z', shiftRu: 'Я',
+      en: 'z', ru: 'я', code: 'KeyZ', enShift: 'Z', ruShift: 'Я',
     },
     {
-      en: 'x', ru: 'ч', code: 'KeyX', shiftEn: 'X', shiftRu: 'Ч',
+      en: 'x', ru: 'ч', code: 'KeyX', enShift: 'X', ruShift: 'Ч',
     },
     {
-      en: 'c', ru: 'с', code: 'KeyC', shiftEn: 'C', shiftRu: 'С',
+      en: 'c', ru: 'с', code: 'KeyC', enShift: 'C', ruShift: 'С',
     },
     {
-      en: 'v', ru: 'м', code: 'KeyV', shiftEn: 'V', shiftRu: 'М',
+      en: 'v', ru: 'м', code: 'KeyV', enShift: 'V', ruShift: 'М',
     },
     {
 
-      en: 'b', ru: 'и', code: 'KeyB', shiftEn: 'B', shiftRu: 'И',
+      en: 'b', ru: 'и', code: 'KeyB', enShift: 'B', ruShift: 'И',
     },
     {
-      en: 'n', ru: 'т', code: 'KeyN', shiftEn: 'N', shiftRu: 'Т',
+      en: 'n', ru: 'т', code: 'KeyN', enShift: 'N', ruShift: 'Т',
     },
     {
-      en: 'm', ru: 'ь', code: 'KeyM', shiftEn: 'M', shiftRu: 'Ь',
+      en: 'm', ru: 'ь', code: 'KeyM', enShift: 'M', ruShift: 'Ь',
     },
     {
-      en: ',', ru: 'б', code: 'Comma', shiftEn: '<', shiftRu: 'Б',
+      en: ',', ru: 'б', code: 'Comma', enShift: '<', ruShift: 'Б',
     },
     {
-      en: '.', ru: 'ю', code: 'Period', shiftEn: '>', shiftRu: 'Ю',
+      en: '.', ru: 'ю', code: 'Period', enShift: '>', ruShift: 'Ю',
     },
     {
-      en: '/', ru: '.', code: 'Slash', shiftEn: '?', shiftRu: ',',
+      en: '/', ru: '.', code: 'Slash', enShift: '?', ruShift: ',',
     },
     {
-      en: 'Shift', ru: 'Shift', code: 'ShiftRight', shiftEn: 'Shift', shiftRu: 'Shift',
+      en: 'Shift', ru: 'Shift', code: 'ShiftRight', enShift: 'Shift', ruShift: 'Shift',
     },
     {
-      en: 'Up', ru: 'Up', code: 'ArrowUp', shiftEn: 'ArrowUp', shiftRu: 'ArrowUp',
+      en: 'Up', ru: 'Up', code: 'ArrowUp', enShift: 'Up', ruShift: 'Up',
     },
     {
-      en: 'Del', ru: 'Del', code: 'Delete', shiftEn: 'Del', shiftRu: 'Del',
+      en: 'Del', ru: 'Del', code: 'Delete', enShift: 'Del', ruShift: 'Del',
     },
   ],
 
   fifthRow: [
     {
-      en: 'Ctrl', ru: 'Ctrl', code: 'ControlLeft', shiftEn: 'Ctrl', shiftRu: 'Ctrl',
+      en: 'Ctrl', ru: 'Ctrl', code: 'ControlLeft', enShift: 'Ctrl', ruShift: 'Ctrl',
     },
     {
-      en: 'Opt', ru: 'Opt', code: 'AltLeft', shiftEn: 'Opt', shiftRu: 'Opt',
+      en: 'Opt', ru: 'Opt', code: 'AltLeft', enShift: 'Opt', ruShift: 'Opt',
     },
     {
-      en: 'Cmd', ru: 'Cmd', code: 'MetaLeft', shiftEn: 'Cmd', shiftRu: 'Cmd',
+      en: 'Cmd', ru: 'Cmd', code: 'MetaLeft', enShift: 'Cmd', ruShift: 'Cmd',
     },
     {
-      en: ' ', ru: ' ', code: 'Space', shiftEn: ' ', shiftRu: ' ',
+      en: ' ', ru: ' ', code: 'Space', enShift: ' ', ruShift: ' ',
     },
     {
-      en: 'Cmd', ru: 'Cmd', code: 'MetaRight', shiftEn: 'Cmd', shiftRu: 'Cmd',
+      en: 'Cmd', ru: 'Cmd', code: 'MetaRight', enShift: 'Cmd', ruShift: 'Cmd',
     },
     {
-      en: 'Opt', ru: 'Opt', code: 'AltRight', shiftEn: 'Opt', shiftRu: 'Opt',
+      en: 'Opt', ru: 'Opt', code: 'AltRight', enShift: 'Opt', ruShift: 'Opt',
     },
     {
-      en: 'Ctrl', ru: 'Ctrl', code: 'ControlRight', shiftEn: 'Ctrl', shiftRu: 'Ctrl',
+      en: 'Ctrl', ru: 'Ctrl', code: 'ControlRight', enShift: 'Ctrl', ruShift: 'Ctrl',
     },
     {
-      en: 'Left', ru: 'Left', code: 'ArrowLeft', shiftEn: 'ArrowLeft', shiftRu: 'ArrowLeft',
+      en: 'Left', ru: 'Left', code: 'ArrowLeft', enShift: 'Left', ruShift: 'Left',
     },
     {
-      en: 'Down', ru: 'Down', code: 'ArrowDown', shiftEn: 'ArrowDown', shiftRu: 'ArrowDown',
+      en: 'Down', ru: 'Down', code: 'ArrowDown', enShift: 'Down', ruShift: 'Down',
     },
     {
-      en: 'Right', ru: 'Right', code: 'ArrowRight', shiftEn: 'ArrowRight', shiftRu: 'ArrowRight',
+      en: 'Right', ru: 'Right', code: 'ArrowRight', enShift: 'Right', ruShift: 'Right',
     },
   ],
 };
+
+export const SPECIAL_KEYS = [
+  'Backspace',
+  'Tab',
+  'CapsLock',
+  'Enter',
+  'ShiftLeft',
+  'ShiftRight',
+  'ControlLeft',
+  'ControlRight',
+  'AltLeft',
+  'AltRight',
+  'MetaLeft',
+  'MetaRight',
+  'Delete',
+  'ArrowUp',
+  'ArrowDown',
+  'ArrowLeft',
+  'ArrowRight',
+];
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './styles.scss';
 
-import keys from './constants';
+import { KEYBOARD_KEYS, SPECIAL_KEYS } from './constants';
 
-const currentLang = localStorage.getItem('lang') || 'en';
+const state = {
+  isUpper: false,
+  currentLang: localStorage.getItem('lang') || 'en',
+};
 
 const createEl = (tag, classNames = [], text = '') => {
   const $el = document.createElement(tag);
@@ -12,10 +15,10 @@ const createEl = (tag, classNames = [], text = '') => {
 };
 
 const createKey = (key) => {
-  const $key = createEl('div', ['keyboard__key'], key[currentLang]);
+  const $key = createEl('div', ['keyboard__key'], key[state.currentLang]);
   $key.dataset.code = key.code;
-  $key.dataset.shiftEn = key.shiftEn;
-  $key.dataset.shiftRu = key.shiftRu;
+  $key.dataset.enShift = key.enShift;
+  $key.dataset.ruShift = key.ruShift;
   $key.dataset.en = key.en;
   $key.dataset.ru = key.ru;
   return $key;
@@ -40,7 +43,7 @@ $container.appendChild($keyboard);
 
 const {
   firstRow, secondRow, thirdRow, fifthRow, fourthRow,
-} = keys;
+} = KEYBOARD_KEYS;
 
 const $firstRow = createRow(firstRow, 'keyboard__row_first');
 const $secondRow = createRow(secondRow, 'keyboard__row_second');
@@ -49,3 +52,121 @@ const $fourthRow = createRow(fourthRow, 'keyboard__row_fourth');
 const $fifthRow = createRow(fifthRow, 'keyboard__row_fifth');
 
 $keyboard.append($firstRow, $secondRow, $thirdRow, $fourthRow, $fifthRow);
+
+const changeCase = () => {
+  const $keys = document.querySelectorAll('.keyboard__key');
+  $keys.forEach((key) => {
+    const $key = key;
+    $key.textContent = state.isUpper ? $key.dataset[`${state.currentLang}Shift`] : $key.dataset[state.currentLang];
+  });
+};
+
+const doSpecialKey = (key) => {
+  switch (key.code) {
+    case 'Backspace':
+      $textarea.value = $textarea.value.slice(0, -1);
+      break;
+    case 'Tab':
+      $textarea.value += '    ';
+      break;
+    case 'Enter':
+      $textarea.value += '\n';
+      break;
+    case 'Space':
+      $textarea.value += ' ';
+      break;
+    case 'Delete':
+      $textarea.value = '';
+      break;
+    case 'ArrowUp':
+      $textarea.value += '↑';
+      break;
+    case 'ArrowDown':
+      $textarea.value += '↓';
+      break;
+    case 'ArrowLeft':
+      $textarea.value += '←';
+      break;
+    case 'ArrowRight':
+      $textarea.value += '→';
+      break;
+    default:
+      break;
+  }
+};
+
+window.addEventListener('keydown', (e) => {
+  e.preventDefault();
+
+  const $key = document.querySelector(`[data-code="${e.code}"]`);
+  if ($key) $key.classList.add('keyboard__key_active');
+
+  if (SPECIAL_KEYS.includes(e.code)) {
+    doSpecialKey(e);
+    return;
+  }
+
+  if (state.isUpper) {
+    $textarea.value += $key.dataset[`${state.currentLang}Shift`];
+    return;
+  }
+
+  $textarea.value += $key.dataset[state.currentLang];
+});
+
+window.addEventListener('keyup', (e) => {
+  e.preventDefault();
+  const $key = document.querySelector(`[data-code="${e.code}"]`);
+  if ($key) $key.classList.remove('keyboard__key_active');
+});
+
+$keyboard.addEventListener('mousedown', (e) => {
+  if (e.target.classList.contains('keyboard__key')) {
+    const $key = e.target;
+    $key.classList.add('keyboard__key_active');
+
+    if (SPECIAL_KEYS.includes($key.dataset.code)) {
+      doSpecialKey($key.dataset);
+      return;
+    }
+
+    if (state.isUpper) {
+      $textarea.value += $key.dataset[`${state.currentLang}Shift`];
+      return;
+    }
+
+    $textarea.value += $key.dataset[state.currentLang];
+  }
+});
+
+$keyboard.addEventListener('mouseup', (e) => {
+  if (e.target.classList.contains('keyboard__key')) {
+    const $key = e.target;
+    $key.classList.remove('keyboard__key_active');
+  }
+});
+
+window.addEventListener('keydown', (e) => {
+  if (e.code === 'CapsLock' || e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
+    state.isUpper = true;
+    changeCase();
+  }
+});
+
+window.addEventListener('keyup', (e) => {
+  if (e.code === 'CapsLock' || e.code === 'ShiftLeft' || e.code === 'ShiftRight') {
+    state.isUpper = false;
+    changeCase();
+  }
+});
+
+$keyboard.addEventListener('click', (e) => {
+  if (e.target.classList.contains('keyboard__key')) {
+    const $key = e.target;
+    if ($key.dataset.code === 'CapsLock' || $key.dataset.code === 'ShiftLeft' || $key.dataset.code === 'ShiftRight') {
+      if (!state.isUpper) $key.classList.add('keyboard__key_active');
+      state.isUpper = !state.isUpper;
+      changeCase();
+    }
+  }
+});
