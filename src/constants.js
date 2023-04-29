0c77f0fc15439c89a169392a64b840a6540caf41
Sergei Andreev <sergey.andreev@dualbootpartners.com>
2023-04-29 02:18:28 +0200

    feat: add keyboard layout

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,212 @@
-export const KEYBOARD_KEYS = [
-    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-    'z', 'x', 'c', 'v', 'b', 'n', 'm',
-    'space', 'backspace', 'enter', 'shift', 'capslock'
-];
+export default {
+  firstRow: [
+    {
+      en: '`', ru: 'ё', code: 'Backquote', shiftEn: '~', shiftRu: 'Ё',
+    },
+    {
+      en: '1', ru: '1', code: 'Digit1', shiftEn: '!', shiftRu: '!',
+    },
+    {
+      en: '2', ru: '2', code: 'Digit2', shiftEn: '@', shiftRu: '"',
+    },
+    {
+      en: '3', ru: '3', code: 'Digit3', shiftEn: '#', shiftRu: '№',
+    },
+    {
+      en: '4', ru: '4', code: 'Digit4', shiftEn: '$', shiftRu: ';',
+    },
+    {
+      en: '5', ru: '5', code: 'Digit5', shiftEn: '%', shiftRu: '%',
+    },
+    {
+      en: '6', ru: '6', code: 'Digit6', shiftEn: '^', shiftRu: ':',
+    },
+    {
+      en: '7', ru: '7', code: 'Digit7', shiftEn: '&', shiftRu: '?',
+    },
+    {
+      en: '8', ru: '8', code: 'Digit8', shiftEn: '*', shiftRu: '*',
+    },
+    {
+      en: '9', ru: '9', code: 'Digit9', shiftEn: '(', shiftRu: '(',
+    },
+    {
+      en: '0', ru: '0', code: 'Digit0', shiftEn: ')', shiftRu: ')',
+    },
+    {
+      en: '-', ru: '-', code: 'Minus', shiftEn: '_', shiftRu: '_',
+    },
+    {
+      en: '=', ru: '=', code: 'Equal', shiftEn: '+', shiftRu: '+',
+    },
+    {
+      en: 'Backspace', ru: 'Backspace', code: 'Backspace', shiftEn: 'Backspace', shiftRu: 'Backspace',
+    },
+  ],
+
+  secondRow: [
+    {
+      en: 'Tab', ru: 'Tab', code: 'Tab', shiftEn: 'Tab', shiftRu: 'Tab',
+    },
+    {
+      en: 'q', ru: 'й', code: 'KeyQ', shiftEn: 'Q', shiftRu: 'Й',
+    },
+    {
+      en: 'w', ru: 'ц', code: 'KeyW', shiftEn: 'W', shiftRu: 'Ц',
+    },
+    {
+      en: 'e', ru: 'у', code: 'KeyE', shiftEn: 'E', shiftRu: 'У',
+    },
+    {
+      en: 'r', ru: 'к', code: 'KeyR', shiftEn: 'R', shiftRu: 'К',
+    },
+    {
+      en: 't', ru: 'е', code: 'KeyT', shiftEn: 'T', shiftRu: 'Е',
+    },
+    {
+      en: 'y', ru: 'н', code: 'KeyY', shiftEn: 'Y', shiftRu: 'Н',
+    },
+    {
+      en: 'u', ru: 'г', code: 'KeyU', shiftEn: 'U', shiftRu: 'Г',
+    },
+    {
+      en: 'i', ru: 'ш', code: 'KeyI', shiftEn: 'I', shiftRu: 'Ш',
+    },
+    {
+      en: 'o', ru: 'щ', code: 'KeyO', shiftEn: 'O', shiftRu: 'Щ',
+    },
+    {
+      en: 'p', ru: 'з', code: 'KeyP', shiftEn: 'P', shiftRu: 'З',
+    },
+    {
+      en: '[', ru: 'х', code: 'BracketLeft', shiftEn: '{', shiftRu: 'Х',
+    },
+    {
+      en: ']', ru: 'ъ', code: 'BracketRight', shiftEn: '}', shiftRu: 'Ъ',
+    },
+    {
+      en: '\\', ru: '\\', code: 'Backslash', shiftEn: '|', shiftRu: '/',
+    },
+  ],
+
+  thirdRow: [
+    {
+      en: 'CapsLock', ru: 'CapsLock', code: 'CapsLock', shiftEn: 'CapsLock', shiftRu: 'CapsLock',
+    },
+    {
+      en: 'a', ru: 'ф', code: 'KeyA', shiftEn: 'A', shiftRu: 'Ф',
+    },
+    {
+      en: 's', ru: 'ы', code: 'KeyS', shiftEn: 'S', shiftRu: 'Ы',
+    },
+    {
+      en: 'd', ru: 'в', code: 'KeyD', shiftEn: 'D', shiftRu: 'В',
+    },
+    {
+      en: 'f', ru: 'а', code: 'KeyF', shiftEn: 'F', shiftRu: 'А',
+    },
+    {
+      en: 'g', ru: 'п', code: 'KeyG', shiftEn: 'G', shiftRu: 'П',
+    },
+    {
+      en: 'h', ru: 'р', code: 'KeyH', shiftEn: 'H', shiftRu: 'Р',
+    },
+    {
+      en: 'j', ru: 'о', code: 'KeyJ', shiftEn: 'J', shiftRu: 'О',
+    },
+    {
+      en: 'k', ru: 'л', code: 'KeyK', shiftEn: 'K', shiftRu: 'Л',
+    },
+    {
+      en: 'l', ru: 'д', code: 'KeyL', shiftEn: 'L', shiftRu: 'Д',
+    },
+    {
+      en: ';', ru: 'ж', code: 'Semicolon', shiftEn: ':', shiftRu: 'Ж',
+    },
+    {
+      en: '\'', ru: 'э', code: 'Quote', shiftEn: '"', shiftRu: 'Э',
+    },
+    {
+      en: 'Enter', ru: 'Enter', code: 'Enter', shiftEn: 'Enter', shiftRu: 'Enter',
+    },
+  ],
+
+  fourthRow: [
+    {
+      en: 'Shift', ru: 'Shift', code: 'ShiftLeft', shiftEn: 'Shift', shiftRu: 'Shift',
+    },
+    {
+      en: 'z', ru: 'я', code: 'KeyZ', shiftEn: 'Z', shiftRu: 'Я',
+    },
+    {
+      en: 'x', ru: 'ч', code: 'KeyX', shiftEn: 'X', shiftRu: 'Ч',
+    },
+    {
+      en: 'c', ru: 'с', code: 'KeyC', shiftEn: 'C', shiftRu: 'С',
+    },
+    {
+      en: 'v', ru: 'м', code: 'KeyV', shiftEn: 'V', shiftRu: 'М',
+    },
+    {
+
+      en: 'b', ru: 'и', code: 'KeyB', shiftEn: 'B', shiftRu: 'И',
+    },
+    {
+      en: 'n', ru: 'т', code: 'KeyN', shiftEn: 'N', shiftRu: 'Т',
+    },
+    {
+      en: 'm', ru: 'ь', code: 'KeyM', shiftEn: 'M', shiftRu: 'Ь',
+    },
+    {
+      en: ',', ru: 'б', code: 'Comma', shiftEn: '<', shiftRu: 'Б',
+    },
+    {
+      en: '.', ru: 'ю', code: 'Period', shiftEn: '>', shiftRu: 'Ю',
+    },
+    {
+      en: '/', ru: '.', code: 'Slash', shiftEn: '?', shiftRu: ',',
+    },
+    {
+      en: 'Shift', ru: 'Shift', code: 'ShiftRight', shiftEn: 'Shift', shiftRu: 'Shift',
+    },
+    {
+      en: 'Up', ru: 'Up', code: 'ArrowUp', shiftEn: 'ArrowUp', shiftRu: 'ArrowUp',
+    },
+    {
+      en: 'Del', ru: 'Del', code: 'Delete', shiftEn: 'Del', shiftRu: 'Del',
+    },
+  ],
+
+  fifthRow: [
+    {
+      en: 'Ctrl', ru: 'Ctrl', code: 'ControlLeft', shiftEn: 'Ctrl', shiftRu: 'Ctrl',
+    },
+    {
+      en: 'Opt', ru: 'Opt', code: 'AltLeft', shiftEn: 'Opt', shiftRu: 'Opt',
+    },
+    {
+      en: 'Cmd', ru: 'Cmd', code: 'MetaLeft', shiftEn: 'Cmd', shiftRu: 'Cmd',
+    },
+    {
+      en: ' ', ru: ' ', code: 'Space', shiftEn: ' ', shiftRu: ' ',
+    },
+    {
+      en: 'Cmd', ru: 'Cmd', code: 'MetaRight', shiftEn: 'Cmd', shiftRu: 'Cmd',
+    },
+    {
+      en: 'Opt', ru: 'Opt', code: 'AltRight', shiftEn: 'Opt', shiftRu: 'Opt',
+    },
+    {
+      en: 'Ctrl', ru: 'Ctrl', code: 'ControlRight', shiftEn: 'Ctrl', shiftRu: 'Ctrl',
+    },
+    {
+      en: 'Left', ru: 'Left', code: 'ArrowLeft', shiftEn: 'ArrowLeft', shiftRu: 'ArrowLeft',
+    },
+    {
+      en: 'Down', ru: 'Down', code: 'ArrowDown', shiftEn: 'ArrowDown', shiftRu: 'ArrowDown',
+    },
+    {
+      en: 'Right', ru: 'Right', code: 'ArrowRight', shiftEn: 'ArrowRight', shiftRu: 'ArrowRight',
+    },
+  ],
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
-import { KEYBOARD_KEYS } from "./constants";
+import './styles.scss';
 
-const bodyEl = document.body;
+import keys from './constants';
 
-const keyboard = document.createElement('div');
-keyboard.classList.add('keyboard');
-bodyEl.appendChild(keyboard);
+const currentLang = localStorage.getItem('lang') || 'en';
 
-const textarea = document.createElement('textarea');
-textarea.classList.add('textarea');
-bodyEl.appendChild(textarea);
+const createEl = (tag, classNames = [], text = '') => {
+  const $el = document.createElement(tag);
+  $el.classList.add(...classNames);
+  if (text) $el.textContent = text;
+  return $el;
+};
 
-console.log(KEYBOARD_KEYS);
+const createKey = (key) => {
+  const $key = createEl('div', ['keyboard__key'], key[currentLang]);
+  $key.dataset.code = key.code;
+  $key.dataset.shiftEn = key.shiftEn;
+  $key.dataset.shiftRu = key.shiftRu;
+  $key.dataset.en = key.en;
+  $key.dataset.ru = key.ru;
+  return $key;
+};
+
+const createRow = (row, className) => {
+  const $row = createEl('div', ['keyboard__row', className]);
+  $row.append(...row.map((key) => createKey(key)));
+  return $row;
+};
+
+const $root = document.querySelector('#root');
+
+const $container = createEl('div', ['container']);
+$root.appendChild($container);
+
+const $textarea = createEl('textarea', ['textarea']);
+$container.appendChild($textarea);
+
+const $keyboard = createEl('div', ['keyboard']);
+$container.appendChild($keyboard);
+
+const {
+  firstRow, secondRow, thirdRow, fifthRow, fourthRow,
+} = keys;
+
+const $firstRow = createRow(firstRow, 'keyboard__row_first');
+const $secondRow = createRow(secondRow, 'keyboard__row_second');
+const $thirdRow = createRow(thirdRow, 'keyboard__row_third');
+const $fourthRow = createRow(fourthRow, 'keyboard__row_fourth');
+const $fifthRow = createRow(fifthRow, 'keyboard__row_fifth');
+
+$keyboard.append($firstRow, $secondRow, $thirdRow, $fourthRow, $fifthRow);
